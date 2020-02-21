a05584da7c0f23620d4cfa6a1345f709f9eea753
Roman Rybachenko <rrybachenko@icloud.com>
2020-02-21 11:45:59 +0200

    initial commit

---
+++ GoogleSignInTest/Classes/Controllers/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GoogleSignInTest
+//
+//  Created by Roman Rybachenko on 21.02.2020.
+//  Copyright © 2020 Roman Rybachenko. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GoogleSignInTest/Classes/Controllers/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GoogleSignInTest
+//
+//  Created by Roman Rybachenko on 21.02.2020.
+//  Copyright © 2020 Roman Rybachenko. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
